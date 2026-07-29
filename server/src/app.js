c50f0f6ef9db0e6a14e1bb4a9a151256e1f773ee
Santosh Shimpi <santoshshimpi2005@gmail.com>
2026-07-29 18:04:30 +0530

    Milestone 2: Setup backend architecture and browser geolocation

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const healthRoutes = require("./routes/healthRoutes");
+const locationRoutes = require("./routes/locationRoutes");
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", healthRoutes);
+app.use("/api", locationRoutes);
+
+app.get("/", (req, res) => {
+    res.send("GeoPulse Backend running");
+});
+
 module.exports = app;
---
+++ server/src/controllers/locationController.js
@@ -0,0 +1,7 @@
+const locationController = (req, res) => {
+    res.json({
+        message: "location received successfully"
+    });
+};
+
+module.exports = locationController;
