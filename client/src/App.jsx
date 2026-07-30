9e1c18085fdb35e2c42638c26a11d765d7c94db6
Santosh Shimpi <santoshshimpi2005@gmail.com>
2026-07-30 18:56:14 +0530

    Milestone 3: Added interactive map with geolocation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,45 @@
-function App() {
-  const getLocation = () => {
+import {MapContainer, TileLayer, 
+Marker, Popup, useMap } from 'react-leaflet';
+import { useState } from 'react';
+function LocationMarker({ location }) {
+  const map = useMap();
+  if (!location) {
+    return null;
+  }
+  console.log(map);
+  map.setView([location.latitude, location.longitude], 15);
+  return (
+    <Marker position={[location.latitude, location.longitude]}>
+      <Popup>
+        You are here: {location.latitude}, {location.longitude}
+      </Popup>
+    </Marker>
+  );
+}
 
+function App() {
+  const [location, setLocation] = useState(null);
+  const getLocation = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log(position);
-      });
+        setLocation(position.coords);
+      }
+    );
     };
     return (
     <div>
       <h1>GeoPulse</h1>
     <button onClick={getLocation}>Get My Location</button>
+    <MapContainer 
+     center={[20.5937, 78.9629]}
+             zoom={5} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <LocationMarker location={location} />
+    </MapContainer>
     </div>
   );
 }
